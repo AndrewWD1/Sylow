9eb373b04e1c05cf9fa399f7f2d1832ec2837fa9
Andrew Doumont <andrew.doumont@gmail.com>
2020-11-06 10:24:02 -0700

    update

--- src/components/sylowResult/SylowResult.tsx
+++ src/components/sylowResult/SylowResult.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import styled from "styled-components";
+import { factor } from "../../utils/factor";
 import { ISylowFactors, sylowFactors } from "../../utils/sylow";
 import { sylowAnalysis } from "../../utils/sylowAnalysis";
 
@@ -7,14 +9,27 @@ interface IProps {
 }
 
 const stringifySylowResult = (p: ISylowFactors) => {
-  let str = `${p.primeFactor}  ->  `;
+  let str = "";
 
   p.sylowFactors.forEach((e) => {
-    str += `{${e}}, `;
+    str += `(${e}), `;
   });
 
   return str.slice(0, str.length - 2);
 };
+
+const Table = styled.table`
+  text-align: left;
+
+  border: 1px solid black;
+  th {
+    border: 1px solid black;
+  }
+  td {
+    padding: 1rem;
+    border: 1px solid black;
+  }
+`;
 
 export const SylowResult: React.FC<IProps> = ({ value }) => {
   const result = sylowFactors(value);
@@ -23,9 +38,29 @@ export const SylowResult: React.FC<IProps> = ({ value }) => {
 
   return (
     <div>
-      {result.map((p) => (
-        <div>{stringifySylowResult(p)}</div>
-      ))}
+      <h2>
+        Sylow subgroup factors for groups of order: {value} =
+        {factor(Number(value)).map((fac) => (
+          <>
+            {" "}
+            {fac[0]}
+            <sup>{fac[1]}</sup>{" "}
+          </>
+        ))}
+      </h2>
+      <Table>
+        <tr>
+          <th>p</th>
+          <th>(p, q mod p)</th>
+        </tr>
+        {result.map((p) => (
+          <tr>
+            <td>{p.primeFactor}</td>
+            <td>{stringifySylowResult(p)}</td>
+          </tr>
+        ))}
+      </Table>
+      <h2>Analysis: </h2>
       <div>{sylowAnalysis(result)}</div>
     </div>
   );
